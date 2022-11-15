97737d9f775e55de19a8e5864ed7529ef730704d
Brian Cameron <bcameron1489@gmail.com>
2022-11-15 17:16:14 -0500

    Add comments to routes.js for context

--- src/catalogs/routes.js
+++ src/catalogs/routes.js
@@ -3,10 +3,16 @@ const controller = require('./controller')
 
 const router = Router();
 
+
+// Get Catlog
 router.get('/', controller.getCatalogs);
+// Create Catalog Item
 router.post('/', controller.addItem);
+// Get Catalog Item by ID
 router.get('/:id', controller.getItemById);
+// Update Catalog Item
 router.put('/:id', controller.updateItem);
+//  Delete Catalog Item
 router.delete('/:id', controller.removeItem);
 
 
