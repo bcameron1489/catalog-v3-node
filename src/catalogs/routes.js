3905ec8bea8cdf1d64af138986d69e7901bd877c
Brian Cameron <bcameron1489@gmail.com>
2022-11-15 15:49:45 -0500

    Add update item route

--- src/catalogs/routes.js
+++ src/catalogs/routes.js
@@ -6,6 +6,7 @@ const router = Router();
 router.get('/', controller.getCatalogs);
 router.post('/', controller.addItem);
 router.get('/:id', controller.getItemById);
+router.put('/:id', controller.updateItem);
 router.delete('/:id', controller.removeItem);
 
 
