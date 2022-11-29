87cf54bc727f3bbdafd4ffcb7444ffe5917b8487
Brian Cameron <bcameron1489@gmail.com>
2022-11-29 11:22:37 -0500

    Add get lists endpoint to lists controller

--- src/lists/controller.js
+++ src/lists/controller.js
@@ -4,8 +4,33 @@ const pool = require('../../db')
 const fetch = require('node-fetch')
 const env = require('../env')
 
+const getLists = (req, res) => {
+    const url = 'https://a.klaviyo.com/api/lists/';
+    const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        revision: '2022-10-17',
+        Authorization: env.auth
+    }
+    };
 
+    fetch(url, options)
+    .then(res => {
+        if (res.ok) {
+            console.log('SUCCESS')
+            return res.json()
+        } else {
+            console.log('REQUEST FAILURE')
+        }
+    })
+    .then(json => {
+        console.log(json)
+        res.status(200).send(json)
+    })
+    .catch(err => console.error('error:' + err));
+}
 
 module.exports = {
-    
+    getLists,
 }
