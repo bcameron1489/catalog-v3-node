22b5263a073194a504fb2053cb46293d68277ad7
Brian Cameron <bcameron1489@gmail.com>
2022-11-29 11:39:05 -0500

    Add get list profile relationships endpoint to lists controller

--- src/lists/controller.js
+++ src/lists/controller.js
@@ -94,11 +94,40 @@ const getListProfiles = (req, res) => {
     .catch(err => console.error('error:' + err));
 }
 
+// Get List Profile Relationships
 
+const getListRelationships = (req, res) => {
+    const id = req.params.id
+    const url = 'https://a.klaviyo.com/api/lists/'+ `${id}` +'/relationships/profiles/';
+    const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        revision: '2022-10-17',
+        Authorization: env.auth
+    }
+    };
+
+    fetch(url, options)
+    .then(res => {
+        if (res.ok) {
+            console.log('SUCCESS')
+            return res.json()
+        } else {
+            console.log('REQUEST FAILURE')
+        }
+    })
+    .then(json => {
+        console.log(json)
+        res.status(200).send(json)
+    })
+    .catch(err => console.error('error:' + err));
+}
 
 
 module.exports = {
     getLists,
     getListById,
     getListProfiles,
+    getListRelationships,
 }
