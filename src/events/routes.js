1615636e11a9d58469ca6c5e52796e1b5c36e4a0
Brian Cameron <bcameron1489@gmail.com>
2022-11-29 10:35:30 -0500

    Add get event profiles route

--- src/events/routes.js
+++ src/events/routes.js
@@ -13,6 +13,8 @@ router.get('/:id', controller.getEventById);
 router.post('/create-event', controller.createEvent);
 // Get Event Metrics
 router.get('/get-event-metrics/:id', controller.getEventMetrics);
+// Get Event Profiles
+router.get('/get-event-profiles/:id', controller.getEventProfiles);
 
 
 module.exports = router
