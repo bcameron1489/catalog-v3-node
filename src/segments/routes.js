932e4b1fdfd7ac0cf4aa0de3f39a151cfdad42ca
Brian Cameron <bcameron1489@gmail.com>
2022-11-30 17:04:29 -0500

    Add get segment profiles to routes

--- src/segments/routes.js
+++ src/segments/routes.js
@@ -8,6 +8,8 @@ const router = Router();
 router.get('/', controller.getSegments);
 // Get Segment By ID
 router.get('/:id', controller.getSegmentById);
+// Get Segment Profiles
+router.get('/get-segment-profiles/:id', controller.getSegmentProfiles);
 
 
 module.exports = router;
