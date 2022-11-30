c0868b6906dad81c072f5df497b872363c07233e
Brian Cameron <bcameron1489@gmail.com>
2022-11-30 16:58:00 -0500

    Add get segments endpoint to segments controller

--- src/segments/controller.js
+++ src/segments/controller.js
@@ -4,9 +4,36 @@ const pool = require('../../db')
 const fetch = require('node-fetch')
 const env = require('../env')
 
+// Get Segments
 
+const getSegments = (req, res) => {
+    const url = 'https://a.klaviyo.com/api/lists/?fields[list]=name,created,updated';
+    const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        revision: '2022-10-17',
+        Authorization: env.auth
+    }
+    };
+
+    fetch(url, options)
+    .then(res => {
+        if (res.ok) {
+            console.log('SUCCESS')
+            return res.json()
+        } else {
+            console.log('REQUEST FAILURE')
+        }
+    })
+    .then(json => {
+        console.log(json)
+        res.status(200).send(json)
+    })
+    .catch(err => console.error('error:' + err));
+}
 
 
 module.exports = {
-    
+    getSegments,
 }
