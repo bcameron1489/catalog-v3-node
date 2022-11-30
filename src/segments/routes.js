a07bd1b7796c4c5ef7ebb0c6147b32918bfd6819
Brian Cameron <bcameron1489@gmail.com>
2022-11-30 17:08:41 -0500

    Add get segment relationships to segment routes

--- src/segments/routes.js
+++ src/segments/routes.js
@@ -10,6 +10,9 @@ router.get('/', controller.getSegments);
 router.get('/:id', controller.getSegmentById);
 // Get Segment Profiles
 router.get('/get-segment-profiles/:id', controller.getSegmentProfiles);
+// Get Segment Relationships
+router.get('/get-segment-relationships', controller.getSegmentRelationships);
+
 
 
 module.exports = router;
