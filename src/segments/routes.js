75a45a72d5f1b9c7ce92169e07f4ccc51f67eae8
Brian Cameron <bcameron1489@gmail.com>
2022-11-30 16:59:33 -0500

    Add get segment by id route

--- src/segments/routes.js
+++ src/segments/routes.js
@@ -5,8 +5,9 @@ const controller = require('./controller');
 const router = Router();
 
 // Get Segments
-
 router.get('/', controller.getSegments);
+// Get Segment By ID
+router.get('/:id', controller.getSegmentById);
 
 
 module.exports = router;
