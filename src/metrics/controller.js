cb1391fdcc350b34eb8ddf73b3d3d2658513190f
Brian Cameron <bcameron1489@gmail.com>
2022-12-01 14:41:34 -0500

    Partially add query metric aggregates call to metrics controller.  Only taking id in the json body for now until the UI is built out to take all the fields

--- src/metrics/controller.js
+++ src/metrics/controller.js
@@ -65,9 +65,57 @@ const getMetricById = (req, res) => {
     .catch(err => console.error('error:' + err));
 }
 
+// Query Metric Aggregates   **  Eventually format to take json body for all fields.  For now just using the metric ID to process the request until UI is built
+
+const aggregates = (req, res) => {
+    const id = req.body.id
+    const url = 'https://a.klaviyo.com/api/metric-aggregates/';
+    const options = {
+    method: 'POST',
+    headers: {
+        accept: 'application/json',
+        revision: '2022-10-17',
+        'content-type': 'application/json',
+        Authorization: env.auth
+    },
+    body: JSON.stringify({
+        data: {
+        type: 'metric-aggregate',
+        attributes: {
+            measurements: ['unique'],
+            filter: [
+            'greater-or-equal(datetime,2022-06-01T00:00:00),less-than(datetime,2022-12-01T00:00:00)'
+            ],
+            metric_id: `${id}`,
+            interval: 'day',
+            page_size: 500,
+            timezone: 'UTC'
+        }
+        }
+    })
+    };
+
+    fetch(url, options)
+    .then(res => {
+        if (res.ok) {
+            console.log('SUCCESS')
+            return res.json()
+        } else {
+            console.log('REQUEST FAILURE')
+        }
+    })
+    .then(json => {
+        console.log(json)
+        res.status(200).send(json)
+    })
+    .catch(err => console.error('error:' + err));
+
+}
+
 
 
 module.exports = {
     getMetrics,
-    getMetricById
+    getMetricById,
+    aggregates
 }
