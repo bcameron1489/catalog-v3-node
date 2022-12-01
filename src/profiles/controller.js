0d309298ebc2c649141fc794ae103c960f2db36d
Brian Cameron <bcameron1489@gmail.com>
2022-12-01 16:17:46 -0500

    Add get profiles endpoint to profiles controller

--- src/profiles/controller.js
+++ src/profiles/controller.js
@@ -4,9 +4,36 @@ const pool = require('../../db')
 const fetch = require('node-fetch')
 const env = require('../env')
 
+// Get Profiles
 
+const getProfiles = (req, res) => {
+    const url = 'https://a.klaviyo.com/api/profiles/?fields[profile]=email,external_id,anonymous_id,first_name&filter=less-than(created,2022-12-01T00:00:00)&sort=-created';
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            revision: '2022-10-17',
+            Authorization: env.auth
+        }
+    }
+
+    fetch(url, options)
+    .then(res => {
+        if (res.ok) {
+            console.log('SUCCESS')
+            return res.json()
+        } else {
+            console.log('REQUEST FAILURE')
+        }
+    })
+    .then(json => {
+        console.log(json)
+        res.status(200).send(json)
+    })
+    .catch(err => console.error('error:' + err));
+}
 
 
 module.exports = {
-    
+    getProfiles
 }
