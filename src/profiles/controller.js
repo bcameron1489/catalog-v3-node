c404d29d5359171fb8604dba8c8f762e80b1d480
Brian Cameron <bcameron1489@gmail.com>
2022-12-01 16:37:19 -0500

    Add get profile lists endpoint to profiles controller

--- src/profiles/controller.js
+++ src/profiles/controller.js
@@ -65,7 +65,39 @@ const getProfileById = (req, res) => {
 }
 
 
+// Get Profile Lists
+
+const getProfileLists = (req, res) => {
+    const id = req.params.id
+    const url = 'https://a.klaviyo.com/api/profiles/'+ `${id}` +'/lists/?fields[list]=name,created,updated';
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            revision: '2022-10-17',
+            Authorization: env.auth
+        }
+    };
+
+    fetch(url, options)
+    .then(res => {
+        if (res.ok) {
+            console.log('SUCCESS')
+            return res.json()
+        } else {
+            console.log('REQUEST FAILURE')
+        }
+    })
+    .then(json => {
+        console.log(json)
+        res.status(200).send(json)
+    })
+    .catch(err => console.error('error:' + err));
+}
+
+
 module.exports = {
     getProfiles,
-    getProfileById
+    getProfileById,
+    getProfileLists,
 }
