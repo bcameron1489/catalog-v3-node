685587897bd85674d5324007dc1117ba8da04a92
Brian Cameron <bcameron1489@gmail.com>
2022-12-01 14:19:44 -0500

    Add get metrics to controller

--- src/metrics/controller.js
+++ src/metrics/controller.js
@@ -6,8 +6,37 @@ const env = require('../env')
 
 
 
+// Get metrics
+
+const getMetrics = (req, res) => {
+    const url = 'https://a.klaviyo.com/api/metrics/?fields[metric]=name,created,updated,integration';
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            revision: '2022-10-17',
+            Authorization: env.auth
+        }
+    };
+
+    fetch(url, options)
+    .then(res => {
+        if (res.ok) {
+            console.log('SUCCESS')
+            return res.json()
+        } else {
+            console.log('REQUEST FAILURE')
+        }
+    })
+    .then(json => {
+        console.log(json)
+        res.status(200).send(json)
+    })
+    .catch(err => console.error('error:' + err));
+}
+
 
 
 module.exports = {
-    
+    getMetrics,
 }
